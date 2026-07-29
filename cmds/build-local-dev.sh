72271ad088a6c58f9fe9d7a48e9e5772cdc63856
Justin Merz <jrmerz@gmail.com>
2026-07-29 14:53:20 -0700

    fixes for local-dev

--- cmds/build-local-dev.sh
+++ cmds/build-local-dev.sh
@@ -1,31 +1,42 @@
 
 #! /bin/bash
-VERSION=$1
-if [[ -z "$VERSION" ]]; then
-  VERSION="main"
-fi
+ALL=false
+VERSION="main"
+
+for arg in "$@"; do
+  case $arg in
+    --all)
+      ALL=true
+      ;;
+    *)
+      VERSION=$arg
+      ;;
+  esac
+done
 
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $ROOT_DIR/..
 
 ENV_FILE=./compose/local-dev/.env
 
-# cork-kube build exec \
-#   -p project-anduin \
-#   -v main \
-#   -o sandbox \
-#   --depth ALL \
-#   --set-env $ENV_FILE
+if [[ "$ALL" == true ]]; then
+  cork-kube build exec \
+    -p project-anduin \
+    -v main \
+    -o sandbox \
+    --depth ALL \
+    --set-env $ENV_FILE
+
+  cork-kube build exec \
+    -p caskfs \
+    -v main \
+    -o sandbox \
+    --set-env $ENV_FILE \
+    --depth ALL
+fi
 
 cork-kube build exec \
-  -p caskfs \
-  -v main \
-  -o sandbox \
+  -p argonath \
+  -v $VERSION \
   --set-env $ENV_FILE \
-  --depth ALL
-
-# cork-kube build exec \
-#   -p argonath \
-#   -v $VERSION \
-#   --set-env $ENV_FILE \
-#   -o sandbox
+  -o sandbox
