447b439f12640e3104f7352cd6efd222b7e8a0d8
Justin Merz <jrmerz@gmail.com>
2026-08-25 20:30:50 -0700

    improvements for dev cask

--- cmds/init.sh
+++ cmds/init.sh
@@ -2,16 +2,23 @@
 
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $ROOT_DIR
+USERNAME=${1:-$(whoami)}
+
+echo "Setting up local development environment for user: $USERNAME"
 
 # Build the fulllocal development environment
-./build-local-dev.sh --all
+# ./build-local-dev.sh --all
 
 # Start the local development environment
 ./up.sh
 
-# install the pg cask tables
+# install the pg cask tables and add yourself as an admin user
 ./argonath-dc.sh exec cask cask init-pg
+./argonath-dc.sh exec cask cask -i admin  acl user-role-set $USERNAME admin
 
-cask env set -t http -h  http://localhost:3001/cask -c dev argo-local-dev
+cask env set -t http -h  http://localhost:4000/cask -c dev argo-local-dev
 cask env default argo-local-dev
+cd ../../argonath/exec
+uv run digtk auth login --cask-env argo-local-dev
+cd $ROOT_DIR
 cask auto-path load ../../argonath/cask/auto-path-rules.json
